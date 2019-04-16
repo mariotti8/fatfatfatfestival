46954a5364d8c3df026d91615f16f46a3c8c3050
mariotti8 <mariotti93@gmail.com>
2019-04-16 19:33:46 +0200

    refactor

--- magnetic-hover/js/index.js
+++ magnetic-hover/js/index.js
@@ -1,138 +1,46 @@
-// var hoverMouse = function($el) {
-//   console.log($el);
 
-//   Object.keys($el).forEach(key => {
-//     var $self = $el[key];
-//     var hover = false;
+const cursorElementsList = [
+  { name: 'text1', speed: 0.1 }, 
+  { name: 'text2', speed: 0.2 }
+];
+const container = document.getElementById("container");
 
+const cursorElements = cursorElementsList.map(element => {
+  return {
+    name: document.querySelector(`.${element.name}`), 
+    speed: element.speed
+  };
+})
 
-//     var offsetHoverMax = $self.getAttribute("offset-hover-max") + 8 || 2.7;
-//     var offsetHoverMin = $self.getAttribute("offset-hover-min") + 8 || 2.5;
-
-//     var attachEventsListener = function() {
-//       document.addEventListener("mousemove", function(e) {
-//         //
-//         var hoverArea = hover ? offsetHoverMax : offsetHoverMin;
-
-//         // cursor
-//         var cursor = {
-//           x: e.clientX,
-//           y: e.clientY
-//         };
-
-//         // size
-//         // var width = $self.offsetWidth;
-//         // var height = $self.offsetHeight;
-//         var width = 80;
-//         var height = 80;
-
-//         // position
-//         var offset = $self.getBoundingClientRect();
-//         var elPos = {
-//           x: offset.left + width,
-//           y: offset.top + height
-//         };
-
-
-//         // comparaison
-//         var x = cursor.x - elPos.x;
-//         var y = cursor.y - elPos.y;
-
-//         // dist
-//         var dist = Math.sqrt(x * x + y * y);
-
-//         // mutex hover
-//         var mutHover = false;
-
-//         // anim
-//         if (dist < width * hoverArea) {
-//           mutHover = true;
-//           if (!hover) {
-//             hover = true;
-//           }
-//           onHover(x, y, key);
-//         }
-
-//         // reset
-//         if (!mutHover && hover) {
-//           onLeave();
-//           hover = false;
-//         }
-//       });
-//     };
-
-//     var onHover = function(x, y, key) {
-//       TweenMax.to($self, 0.4 + 1, {
-//         x: x * 0.8,
-//         y: y * 0.8,
-//         //scale: .9,
-//         rotation: 0,
-//       });
-//     };
-//     var onLeave = function() {
-//       TweenMax.to($self, 0.7 + parseInt(key, 10), {
-//         x: 0,
-//         y: 0,
-//         scale: 1,
-//         rotation: 0,
-//         // ease: Elastic.easeOut.config(1.2, 0.4)
-//       });
-//       console.log('leave');
-//     };
-
-//     attachEventsListener();
-//   });
-// };
-
-// const el = document.getElementsByClassName('hover_button');
-
-// hoverMouse(el);
-
-
-
-var container = document.getElementById("container");
-var circle1 = document.querySelector(".circle1");
-var circle2 = document.querySelector(".circle2");
-
-TweenMax.set(circle1, {xPercent: -50, yPercent: -50 });
-TweenMax.set(circle2, {xPercent: -50, yPercent: -50 });
+cursorElements.forEach(element => {
+  TweenMax.set(element.name, { 
+    xPercent: -50, 
+    yPercent: -50 
+  });
+});
 
 container.addEventListener("pointerenter", e => {
-  TweenMax.to(circle1, 0.2, {top: 0, left: 0 });
-  TweenMax.to(circle2, 0.1, {top: 0, left: 0 });
+  cursorElements.forEach(element => {
+    TweenMax.to(element.name, element.speed, 
+      { top: 0, 
+        left: 0 
+      });
+  });
   positionCircle(e);
 });
 
 container.addEventListener("pointerleave", e => {
-  // TweenMax.to(circle1, 0.7, {
-  //   x: 0,
-  //   y: 0,
-  //   // top: '50%', 
-  //   // scale: 1,
-  //   top: 'auto',
-  //   left: 'auto',
-  //   transition: 'none',
-  //   scale: 1,
-  //   // rotation: 0,
-  //   // ease: Elastic.easeOut.config(1.2, 0.4)
-  // });
   console.log("leave");
-  
-  TweenMax.to([circle2, circle1], 1.5, {
-    
-    top: '50%',
-    left: '50%',
-    transition: 'none',
-    // xPercent: -50,
-    scale: 1,
-    x: 0,
-    y: 0,
-    // rotation: 0,
-    // ease: Elastic.easeOut.config(1.2, 0.4)
-  });
-  // TweenMax.to(circle1, 0.2, { scale: 0, opacity: 0 });
-  // TweenMax.to(circle2, 0.1, { scale: 0, opacity: 0 });
-  // positionCircle(e);
+  cursorElements.forEach(element => {
+    TweenMax.to(element.name, 1.5, {
+      top: '50%',
+      left: '50%',
+      transition: 'none',
+      scale: 1,
+      x: 0,
+      y: 0,
+    });
+  })
 });
 
 container.addEventListener("pointermove", e => {
@@ -140,11 +48,13 @@ container.addEventListener("pointermove", e => {
 });
 
 function positionCircle(e) {
-  var rect = container.getBoundingClientRect();
   var relX = e.pageX - container.offsetLeft;
   var relY = e.pageY - container.offsetTop;
-
-  TweenMax.to(circle1, 0.2, {x: relX, y: relY });
-  TweenMax.to(circle2, 0.1, {x: relX, y: relY });
+  cursorElements.forEach(element => {
+    TweenMax.to(element.name, element.speed, { 
+      x: relX, 
+      y: relY 
+    });
+  });
 }
 
