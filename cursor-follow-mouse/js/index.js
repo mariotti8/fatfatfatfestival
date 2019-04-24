a6eb19c27de508a584ac25fcc21134bf10ecf536
mariotti8 <mariotti93@gmail.com>
2019-04-24 19:07:11 +0200

    feat: added cursor png

--- cursor-follow-mouse/js/index.js
+++ cursor-follow-mouse/js/index.js
@@ -3,7 +3,6 @@
 const cursor = document.getElementById('cursor');
 
 document.addEventListener('mousemove', e => {
-  console.log("move")
    document.getElementById('cursor').style.left = e.pageX - 0 + 'px';
    document.getElementById('cursor').style.top = e.pageY - 0 + 'px' ;
 })
